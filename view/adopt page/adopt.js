90e9dd3dda31ac8c068bcb7004c6d4383a06d5a2
Esraa Ramadan <104277330+esraa014@users.noreply.github.com>
2022-12-26 23:18:58 +0200

    Add files via upload
    finished adopt page hard coded

---
+++ view/adopt page/adopt.js
@@ -0,0 +1,11 @@
+// const main=document.querySelector("#maincontent")
+// function buildcolumn (text,img ){
+//     const column = document.createElement("div") 
+//     column.classList.add("column1")
+//     column.innerText=text
+//     column.innerHTML
+//     main.appendChild(column)
+// }
+// buildcolumn("ggdtg")
+// const data =["dfrfr","dfrfr","dfrfr","dfrfr","dfrfr","dfrfr",]
+// data.map(e=>buildcolumn(e))
